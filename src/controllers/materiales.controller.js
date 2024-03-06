c52a8dc5dc37c6badc17ad0ed1db747a324c67f3
Benja Psinet <benjamca@icloud.com>
2024-03-06 19:09:19 -0300

    Modificando query

--- src/controllers/materiales.controller.js
+++ src/controllers/materiales.controller.js
@@ -15,7 +15,24 @@ const getMaterial = async (req, res) => {
     try {
         const conn = await pool.getConnection()
 
-        const result = await conn.query('SELECT * FROM articulo WHERE nombre LIKE \'%\' ? \'%\' OR sku LIKE  \'%\' ? \'%\'',[search_value, search_value])
+        const result_entrada = await conn.query('SELECT articulo.nombre, articulo.sap, articulo.codigo_interno, articulo.sku, SUM(detalle_ticket_entrada.cantidad) AS cantidad_entrada '+ 
+        'FROM articulo INNER JOIN  detalle_ticket_entrada ON articulo.idarticulo = detalle_ticket_entrada.articulo_idarticulo '+
+        'WHERE nombre LIKE \'%\' ? \'%\' OR sku LIKE  \'%\' ? \'%\'',[search_value, search_value])
+
+        const result_salida = await conn.query('SELECT articulo.nombre, articulo.sap, articulo.codigo_interno, articulo.sku, SUM(detalle_ticket_salida.cantidad) AS cantidad_salida '+ 
+        'FROM articulo INNER JOIN detalle_ticket_salida ON articulo.idarticulo = detalle_ticket_salida.articulo_idarticulo '+
+        'WHERE nombre LIKE \'%\' ? \'%\' OR sku LIKE  \'%\' ? \'%\'',[search_value, search_value])
+
+        console.log(result_salida)
+
+        const result =  {
+        nombre: result_entrada.nombre,
+        sap: result_entrada.sap,
+        codigo_interno: result_entrada.codigo_interno,
+        sku: result_entrada.sku,
+        cantidad: result_entrada.cantidad + result_salida.cantidad}
+
+        console.log(result)
 
         if (result.length === 0) { conn.end(); return res.status(404).json({ message: "Material no encontrado" });   }
 
--- src/helpers/convertToObject.js
+++ src/helpers/convertToObject.js
@@ -1,11 +1,19 @@
 
 
- const toObject = (result) => {
-    return JSON.parse(JSON.stringify(result, (key, value) =>
+//  const toObject = (result) => {
+//     return JSON.parse(JSON.stringify(result, (key, value) =>
+//         typeof value === 'bigint'
+//             ? Number(value)
+//             : value // return everything else unchanged
+//     ));
+// }
+
+const toObject = (result) => {
+    return result, (key, value) =>
         typeof value === 'bigint'
             ? Number(value)
             : value // return everything else unchanged
-    ));
+    ;
 }
 
 module.exports = {toObject};
