dfbaf6f4a6771f96116f237ce1ce612b38fb4edb
Benjamin Cortes <benjamca@hotmail.com>
2024-04-21 01:11:51 -0400

    Registro user terminado

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -3,8 +3,6 @@ const pool = require('../db')
 const getUsuarios = async(req, res) => {
     const tipo_usuario = req.params.id
 
-   
-
     try {
         const conn = await pool.getConnection()
         const result = await conn.query('SELECT idusuario AS id,idusuario AS value, nombre AS label,correo,usuario FROM usuario WHERE estado_usuario_idestado_usuario = ?', [tipo_usuario])
@@ -14,10 +12,29 @@ const getUsuarios = async(req, res) => {
 
 
     } catch (error) {
-        res.status(400).send('hubo un error en getUsuarios: ' + error)
+        conn.end()
+        return res.status(400).send('hubo un error en getUsuarios: ' + error)
     }
 }
 
+const getTypesUsuarios = async(req, res) => {
+   
+    try {
+        const conn = await pool.getConnection()
+        const result = await conn.query('SELECT * FROM estado_usuario')
+      
+        conn.end()
+
+        res.status(200).json(result)
+    } catch (error) {
+       
+        return res.status(400).send('hubo un error en getTypesUsuarios: ' + error)
+       
+    }
+
+}
+
 module.exports = {
-getUsuarios
+getUsuarios,
+getTypesUsuarios
 }
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,11 +1,16 @@
 const { Router } = require('express')
 const { authRequired } = require('../middlewares/validateToken')
 
-const { getUsuarios } = require('../controllers/usuarios.controller')
+const { getUsuarios, getTypesUsuarios } = require('../controllers/usuarios.controller')
 
 const router = Router()
 
+//OBTENER TIPOS DE USUARIO
+router.get('/usuarios/types', getTypesUsuarios)
+
 //LISTAR USUARIOS POR TIPO DE USUARIO
 router.get('/usuarios/:id', authRequired, getUsuarios)
+
+
 
 module.exports = router
