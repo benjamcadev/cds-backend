04235e2cf579c42299608e086e644f1a67ea36ac
Benjamin Cortes <benjamca@hotmail.com>
2024-04-23 23:18:49 -0400

    Agregando ruta de verify token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const pool = require('../db')
 const bcrypt = require('bcryptjs')
 const { createAccessToken } = require("../helpers/jwt")
 const { convertBigintToInt } = require('../helpers/convertBigintToInt')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const register = async (req, res) => {
@@ -33,7 +34,7 @@ const register = async (req, res) => {
             if (resultNewUser.affectedRows >= 1) {
                 //CREAR TOKEN
                 const token = await createAccessToken({ id: convertBigintToInt(resultNewUser.insertId) })
-                
+
                 const resultUser = await conn.query('SELECT * FROM usuario WHERE idusuario = ?', [resultNewUser.insertId])
 
 
@@ -79,7 +80,7 @@ const login = async (req, res) => {
 
         if (resultUser.length == 0) {
             //USUARIO NO EXISTE
-           return res.status(400).json({
+            return res.status(400).json({
                 message: 'Usuario no esta registrado.'
             })
         } else {
@@ -88,24 +89,24 @@ const login = async (req, res) => {
             //COMPARANDO PASS
             const passMatch = await bcrypt.compare(pass, resultUser[0].pass)
 
-            if (!passMatch) {  
-               return res.status(400).json({
+            if (!passMatch) {
+                return res.status(400).json({
                     message: 'Contraseña incorrecta.'
                 })
             }
 
-             //CREAR TOKEN
-             const token = await createAccessToken({ id: convertBigintToInt(resultUser[0].idusuario) })
+            //CREAR TOKEN
+            const token = await createAccessToken({ id: convertBigintToInt(resultUser[0].idusuario) })
 
-           //GUARDAMOS EL TOKEN EN UNA COOKIE
-           res.cookie('token', token)
-           res.status(200).json({
-               message: 'Usuario logeado exitosamente',
-               id: resultUser[0].idusuario,
-               nombre: resultUser[0].nombre,
-               correo: resultUser[0].correo,
-               usuario: resultUser[0].usuario
-           })
+            //GUARDAMOS EL TOKEN EN UNA COOKIE
+            res.cookie('token', token)
+            res.status(200).json({
+                message: 'Usuario logeado exitosamente',
+                id: resultUser[0].idusuario,
+                nombre: resultUser[0].nombre,
+                correo: resultUser[0].correo,
+                usuario: resultUser[0].usuario
+            })
 
 
 
@@ -127,8 +128,37 @@ const logout = async (req, res) => {
     })
 }
 
+const verifyToken = async (req, res) => {
+
+    const { token } = req.cookies
+
+    if (!token) return res.status(401).json({ message: "No Autorizado" })
+
+    jwt.verify(token, process.env.PASS_TOKEN, async (err, user) => {
+
+
+        if (err) return res.status(401).json({ message: "No Autorizado" });
+
+        const conn = await pool.getConnection()
+        const userFound = await conn.query('SELECT * FROM usuario WHERE idusuario = ?', [user.id])
+       
+        conn.end()
+        if (userFound.length == 0) {
+            res.status(401).json({ message: "No Autorizado" });
+        }
+
+       return res.status(200).json({
+            id: userFound[0].idusuario,
+            nombre: userFound[0].nombre,
+            correo: userFound[0].correo,
+            usuario: userFound[0].usuario
+        })
+    })
+}
+
 module.exports = {
     register,
     login,
-    logout
+    logout,
+    verifyToken
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { register , login, logout } = require('../controllers/auth.controller')
+const { register , login, logout, verifyToken } = require('../controllers/auth.controller')
 const { validateSchema } = require('../middlewares/validateSchemas')
 const { registerSchema, loginSchema } = require('../schemas/auth.schema')
 const router = Router()
@@ -9,6 +9,8 @@ router.post('/auth/register', validateSchema(registerSchema), register)
 router.post('/auth/login',validateSchema(loginSchema), login)
 
 router.post('/auth/logout', logout)
+
+router.get('/auth/verify', verifyToken)
 
 
 module.exports = router
