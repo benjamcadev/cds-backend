e522122068dff72d21074cae8f9df11de725c14f
francodev7 <francisco.pereira16@inacapmail.cl>
2024-08-13 16:26:27 -0400

    fix size index.js

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const app = express()
 
 
 // Configurar el límite de tamaño del cuerpo de la solicitud
-app.use(express.json({ limit: '2mb' }));
-app.use(express.urlencoded({ limit: '2mb', extended: true }));
+app.use(express.json({ limit: '8mb' }));
+app.use(express.urlencoded({ limit: '8mb', extended: true }));
 
 
 
